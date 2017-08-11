ea3efa9929db1336ae30b19b036ca844639b3d20
schaed <schae@lxplus006.cern.ch>
2017-08-11 13:58:46 +0200

    =adding code for the SPIDR board

---
+++ run.sh
@@ -0,0 +1,11 @@
+#root -q -l fitSPIDR.C++\(\"/tmp/data_Ca\",\"\"\)
+#root -q -l fitSPIDR.C++\(\"/tmp/data_Cu\",\"\"\)
+#root -q -l fitSPIDR.C++\(\"/tmp/data_Sn\",\"\"\)
+#root -q -l fitSPIDR.C++\(\"/tmp/data_Ti\",\"\"\)
+#root -q -l fitSPIDR.C++\(\"/tmp/data_Zr\",\"\"\)
+root -q -l fitSPIDR.C++\(\"/tmp/data_GaAs\",\"\"\)
+
+
+
+#/tmp/data_0.dat   /tmp/data_0_sort.dat  /tmp/data_Ca_sort.dat  /tmp/data_Cu_sort.dat  /tmp/data_GaAs_sort.dat  /tmp/data_Sn_sort.dat  /tmp/data_Ti_sort.dat  /tmp/data_Zr_sort.dat
+#/tmp/data_0.root  /tmp/data_Ca.dat      /tmp/data_Cu.dat       /tmp/data_GaAs.dat     /tmp/data_Sn.dat         /tmp/data_Ti.dat       /tmp/data_Zr.dat
